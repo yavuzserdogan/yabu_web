074c7e9e257a5fa3306a373c98b4bdd18d85986d
Yavuz Selim Erdoğan <yavuz.selimm.erdogan@gmail.com>
2026-02-19 01:28:18 +0300

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sybordigital.com'
+  const baseUrl = 'https://www.sybordigital.com'
 
   return [
     {
